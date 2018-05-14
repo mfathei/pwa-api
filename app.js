26843fa81d751f73aefa511aea2385f16d16fd3c
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-14 15:30:36 -0400

    upload picture and save post

--- app.js
+++ app.js
@@ -3,66 +3,150 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var formidable = require('formidable');
+var fs = require('fs');
 
 var Genre = require('./models/genre');
 var Book = require('./models/book');
+var Post = require('./models/post');
 
 // to parse body for POST requests
 app.use(bodyParser.json());
+// Allow CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Connect to Mongoose
 mongoose.connect("mongodb://localhost/bookstore");
 var db = mongoose.connection;
 
-app.get('/', function (req , res){
+app.get('/', function (req, res) {
     res.send("Please use /api/posts");
+});
+
+
+// ============= posts =================
+
+app.get('/api/posts', function (req, res) {
+    Post.getPosts(function (err, posts) {
+        if (err) {
+            throw err;
+        }
+        res.status(200).json(posts);
+    });
+});
+
+app.get('/api/posts/:_id', function (req, res) {
+    Post.getPostById(req.params._id, function (err, post) {
+        if (err) {
+            throw err;
+        }
+        res.status(200).json(post);
+    });
+});
+
+app.post('/api/posts', function (req, res) {
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.file.path;
+        var newpath = '/tmp/pwa-api/' + files.file.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            // res.write('File uploaded and moved!');
+            // res.end();
+        });
+
+        var post = {
+            id: fields.id,
+            title: fields.title,
+            location: fields.location,
+            image: '/src/images/sf-boat.jpg'// encodeURIComponent(file.name)
+        };
+        console.log('post', post);
+        Post.addPost(post, function (err, post) {
+            if (err) {
+                throw err;
+            }
+            res.status(201).json(post);
+        });
+    });
+
+});
+
+app.put('/api/posts/:_id', function (req, res) {
+    var id = req.params._id;
+    var post = req.body;
+    Post.updatePost(id, post, {}, function (err, post) {
+        if (err) {
+            throw err;
+        }
+        res.status(202).json(post);
+    });
+});
+
+app.delete('/api/posts/:_id', function (req, res) {
+    var id = req.params._id;
+    Post.removePost(id, function (err, post) {
+        if (err) {
+            throw err;
+        }
+        res.status(204).json(post);
+    });
+});
+
+app.post('/api/storePostData', function (req, res) {
+
 });
 
 // ============= genres ================
 
-app.get('/api/genres', function(req, res){
-    Genre.getGenres(function(err, genres){
-        if(err){
+app.get('/api/genres', function (req, res) {
+    Genre.getGenres(function (err, genres) {
+        if (err) {
             throw err;
         }
         res.json(genres);
     });
 });
 
-app.get('/api/genres/:_id', function(req, res){
-    Genre.getGenreById(req.params._id, function(err, genre){
-        if(err){
+app.get('/api/genres/:_id', function (req, res) {
+    Genre.getGenreById(req.params._id, function (err, genre) {
+        if (err) {
             throw err;
         }
         res.json(genre);
     });
 });
 
-app.post('/api/genres', function(req, res){
+app.post('/api/genres', function (req, res) {
     var genre = req.body;
-    Genre.addGenre(genre, function(err, genre){
-        if(err){
+    Genre.addGenre(genre, function (err, genre) {
+        if (err) {
             throw err;
         }
         res.json(genre);
     });
 });
 
-app.put('/api/genres/:_id', function(req, res){
+app.put('/api/genres/:_id', function (req, res) {
     var id = req.params._id;
     var genre = req.body;
-    Genre.updateGenre(id, genre, {}, function(err, genre){
-        if(err){
+    Genre.updateGenre(id, genre, {}, function (err, genre) {
+        if (err) {
             throw err;
         }
         res.json(genre);
     });
 });
 
-app.delete('/api/genres/:_id', function(req, res){
+app.delete('/api/genres/:_id', function (req, res) {
     var id = req.params._id;
-    Genre.removeGenre(id, function(err, genre){
-        if(err){
+    Genre.removeGenre(id, function (err, genre) {
+        if (err) {
             throw err;
         }
         res.json(genre);
@@ -71,49 +155,49 @@ app.delete('/api/genres/:_id', function(req, res){
 
 // ============= books ================
 
-app.get('/api/books', function(req, res){
-    Book.getBooks(function(err, books){
-        if(err){
+app.get('/api/books', function (req, res) {
+    Book.getBooks(function (err, books) {
+        if (err) {
             throw err;
         }
         res.json(books);
     });
 });
 
-app.get('/api/books/:_id', function(req, res){
-    Book.getBookById(req.params._id, function(err, book){
-        if(err){
+app.get('/api/books/:_id', function (req, res) {
+    Book.getBookById(req.params._id, function (err, book) {
+        if (err) {
             throw err;
         }
         res.json(book);
     });
 });
 
-app.post('/api/books', function(req, res){
+app.post('/api/books', function (req, res) {
     var book = req.body;
-    Book.addBook(book, function(err, book){
-        if(err){
+    Book.addBook(book, function (err, book) {
+        if (err) {
             throw err;
         }
         res.json(book);
     });
 });
 
-app.put('/api/books/:_id', function(req, res){
+app.put('/api/books/:_id', function (req, res) {
     var id = req.params._id;
     var book = req.body;
-    Book.updateBook(id, book, {}, function(err, book){
-        if(err){
+    Book.updateBook(id, book, {}, function (err, book) {
+        if (err) {
             throw err;
         }
         res.json(book);
     });
 });
 
-app.delete('/api/books/:_id', function(req, res){
+app.delete('/api/books/:_id', function (req, res) {
     var id = req.params._id;
-    Book.removeBook(id, function(err, book){
-        if(err){
+    Book.removeBook(id, function (err, book) {
+        if (err) {
             throw err;
         }
         res.json(book);
---
+++ models/post.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+
+// Genre Schema 
+// _id is auto added in MongoDB but id is needed for client side indexedDB storage
+var postSchema = mongoose.Schema({
+    id: {
+        type: String,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: '/src/images/sf-boat.jpg'
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Post = module.exports = mongoose.model('Post', postSchema);
+
+// GET Posts
+module.exports.getPosts = function (callback, limit) {
+    Post.find(callback).limit(limit);
+}
+
+// GET Post
+module.exports.getPostById = function (id, callback) {
+    Post.findById(id, callback);
+}
+
+// Add Post
+module.exports.addPost = function (post, callback) {
+    Post.create(post, callback);
+}
+
+// Update Post
+module.exports.updatePost = function (id, post, options, callback) {
+    var query = { _id: id };
+    var update = {
+        name: post.name
+    }
+    Post.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Post
+module.exports.removePost = function (id, callback) {
+    var query = { _id: id };
+    Post.remove(query, callback);
+}
